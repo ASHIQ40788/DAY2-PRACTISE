3c932e168f07ed70a109cc294abfd9b0b78abc25
ShaikAshiq <ashiqshaiksvali8@gmail.comgit config --global user.email ashiqshaiksvali8@gmail.com>
2021-08-30 20:03:36 +0530

    code executed

--- tosscoin.java
+++ tosscoin.java
@@ -1,12 +1,24 @@
-import java.util.Scanner;
 import java.util.Random;
 
-public class tosscoin
-{
-    public static void main(String[] args)
-           {
-        
-        
-              }
+public class CoinToss
+ {
+    private enum Coin {Heads, Tails};
 
+    Random randomNum = new Random();
+    private int result = randomNum.nextInt(2);
+    private int heads = 0;
+    private int tails = 1;
+    Coin coinFlip;
+
+    public void flip() 
+{
+        if (result == 0) 
+{
+            coinFlip = Coin.Heads;
+            System.out.println("You flipped Heads!");
+        } else {
+            coinFlip = Coin.Tails;
+            System.out.println("You flipped Tails!");
+        }
+    }
 }
